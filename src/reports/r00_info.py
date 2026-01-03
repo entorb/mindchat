208c433a20dfe9761b1736a6f7d5c5e0a847adb0
Torben <59419684+entorb@users.noreply.github.com>
2026-01-03 11:40:15 +0100

    Minor text

--- src/reports/r00_info.py
+++ src/reports/r00_info.py
@@ -6,24 +6,26 @@ st.markdown(
     """
 # Info
 
+Falls auf dem Handy die Navigation links nicht angezeigt wird, oben links auf den Pfeil klicken.
+
 ## Haftungsausschluss
 
 Dies ist eine KI-basierte Chat-Anwendung, die Anreize und Tipps geben könnte. Sie ersetzt **NICHT** professionelle Hilfe. Bei ernsthaften Problemen wende Dich bitte an qualifizierte Fachkräfte, zB unter 0800 - 111 0 111 (Telefonseelsorge)
 
 ## Funktionsweise
 
-- Ausfüllen einer Selbstauskunft
-- Chat mit einem KI-'Therapeuten', der Deine Selbstauskunft als Kontext für Chat und Vorschläge verwendet
+1. Ausfüllen einer Selbstauskunft
+2. Chat mit einem KI-'Therapeuten', der Deine Selbstauskunft als Kontext für Chat und Vorschläge verwendet
 
 ## Datenschutz
 
 - Deine Daten werden nur im Speicher verarbeitet und nicht auf dem Server oder in einer Datenbank gespeichert. Alle Daten werden beim Logout und Sitzungsende gelöscht.
-- **Achtung:** Deine Daten werden von der OpenAI KI über deren API verarbeitet.
-- Die Kommunikation ist über HTTPS verschlüsselt.
-- Der Quellcode dieser App ist öffentlich auf [GitHub](https://github.com/entorb/mindchat) verfügbar.
+- **Achtung:** Deine Daten werden von der OpenAI KI über deren API auf deren Server verarbeitet.
+- Vorteil gegenüber einem persönlichem ChatGPT Account etc.: OpenAI kann Deine Daten nicht Deiner Person, sondern nur dieser App zuordnen.
+- Der Quellcode dieser App ist öffentlich auf [GitHub](https://github.com/entorb/mindchat) verfügbar, so dass jeder ihn prüfen kann.
 
 ## Technologie
 
-Die Anwendung verwendet Python 3.11, Streamlit, UV, SonarQube für statische Code-Analyse, Matomo für Zugriffsstatistiken (ohne personenbezogene Daten) und Sentry für Fehlermeldungen (ohne Benutzerdaten).
+Diese Anwendung ist in Python Streamlit geschrieben und verwendet diese externen Dienst: OpenAI für KI, SonarQube für Code-Analyse, Matomo für Zugriffsstatistiken (lokale Instanz ohne personenbezogene Daten) und Sentry für Fehler-Tracking (ohne personenbezogene Daten).
         """  # noqa: E501
 )
--- src/reports/r01_self.py
+++ src/reports/r01_self.py
@@ -11,10 +11,10 @@ def main() -> None:  # noqa: D103
     st.title("Selbstauskunft")
 
     st.markdown("""
-- Fülle diese Selbstauskunft aus, damit die KI Dich beraten kann
-- Zeilen mit '##' sind Überschriften, füge gerne weitere ein oder lösche irrelevante
-- Klick Button "Speichen" um die Daten zu übernehmen
-- Tipp: Falls Du diese App nochmal verwenden willst, kopiere und speichere den Text hinterher auf deinem Gerät, da beim Abmelden alle Deinen Daten vom Server gelöscht werden.""")  # noqa: E501
+- Fülle diese Selbstauskunft aus, damit Du mit der KI über Dich chatten kannst
+- Zeilen mit '##' sind Überschriften, lösche gerne irrelevante oder füge weitere ein
+- Wichtig: "Speichen" um die Daten zu übernehmen
+- Tipp: Falls Du diese App nochmal verwenden willst, kopiere und speichere den Text hinterher auf dein Gerät, da beim Abmelden alle Deinen Daten vom Server gelöscht werden.""")  # noqa: E501
 
     if "my-self-disclosure" in st.session_state:
         self_disclosure = st.session_state["my-self-disclosure"]
@@ -23,9 +23,9 @@ def main() -> None:  # noqa: D103
         self_disclosure = Path("src/prompts/self-disclosure-template.md").read_text()
 
     text_content = st.text_area(
-        label="",
+        label="Selbstauskunft",
         value=self_disclosure,
-        height=400,  # fallback height
+        height=800,  # fallback height
         placeholder="Enter your text here...",
     )
     # 2nd save button to prevent data loss
@@ -43,9 +43,13 @@ def main() -> None:  # noqa: D103
             processed_lines.append(stripped)
         text_content = "\n".join(processed_lines)
 
-        st.session_state["my-self-disclosure"] = text_content
+        if text_content != "":
+            st.session_state["my-self-disclosure"] = text_content
 
-    if st.session_state["my-self-disclosure"] != "":
+    if (
+        "my-self-disclosure" in st.session_state
+        and st.session_state["my-self-disclosure"] != ""
+    ):
         st.header("Feedback")
         btn_feedback = st.button("KI Feedback einholen")
 
