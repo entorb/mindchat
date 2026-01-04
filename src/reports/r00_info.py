30e19cc88b5a669d16e3de61a74cfff4ccd9144c
Torben <59419684+entorb@users.noreply.github.com>
2026-01-04 11:19:55 +0100

    Removed Matomo and Sentry

--- src/reports/r00_info.py
+++ src/reports/r00_info.py
@@ -10,7 +10,7 @@ Falls auf dem Handy die Navigation links nicht angezeigt wird, oben links auf de
 
 ## Haftungsausschluss
 
-Dies ist eine KI-basierte Chat-Anwendung, die Anreize und Tipps geben könnte. Sie ersetzt **NICHT** professionelle Hilfe. Bei ernsthaften Problemen wende Dich bitte an qualifizierte Fachkräfte, zB unter 0800 - 111 0 111 (Telefonseelsorge)
+Dies ist eine KI-basierte Chat-Anwendung, die Anreize und Tipps geben kann. Sie ersetzt **NICHT** professionelle Hilfe. Bei ernsthaften Problemen wende Dich bitte an qualifizierte Fachkräfte, z.B. unter 0800 - 111 0 111 (Telefonseelsorge)
 
 ## Funktionsweise
 
@@ -22,10 +22,6 @@ Dies ist eine KI-basierte Chat-Anwendung, die Anreize und Tipps geben könnte. S
 - Deine Daten werden nur im Speicher verarbeitet und nicht auf dem Server oder in einer Datenbank gespeichert. Alle Daten werden beim Logout und Sitzungsende gelöscht.
 - **Achtung:** Deine Daten werden von der OpenAI KI über deren API auf deren Server verarbeitet.
 - Vorteil gegenüber einem persönlichem ChatGPT Account etc.: OpenAI kann Deine Daten nicht Deiner Person, sondern nur dieser App zuordnen.
-- Der Quellcode dieser App ist öffentlich auf [GitHub](https://github.com/entorb/mindchat) verfügbar, so dass jeder ihn prüfen kann.
-
-## Technologie
-
-Diese Anwendung ist in Python Streamlit geschrieben und verwendet diese externen Dienst: OpenAI für KI, SonarQube für Code-Analyse, Matomo für Zugriffsstatistiken (lokale Instanz ohne personenbezogene Daten) und Sentry für Fehler-Tracking (ohne personenbezogene Daten).
+- Der Quellcode dieser App ist öffentlich auf [GitHub](https://github.com/entorb/mindchat) einsehbar, so dass jeder ihn prüfen (und verbessern) kann.
         """  # noqa: E501
 )
