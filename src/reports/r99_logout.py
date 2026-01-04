82f46a19ec05ffdacafceebeb268b033f1dbc337
Torben <59419684+entorb@users.noreply.github.com>
2026-01-04 20:05:23 +0100

    Refactoring: all texts into text.py

--- src/reports/r99_logout.py
+++ src/reports/r99_logout.py
@@ -2,6 +2,8 @@
 
 import streamlit as st
 
+from text import r99_logout
+
 
 def main() -> None:  # noqa: D103
     st.title("Logout")
@@ -11,9 +13,7 @@ def main() -> None:  # noqa: D103
     for key in keys_to_delete:
         del st.session_state[key]
 
-    st.write(
-        "Abgemeldet: Deine Daten wurden aus dem Arbeitsspeicher des Servers gelöscht"
-    )
+    st.write(r99_logout)
 
 
 if __name__ == "__main__":
