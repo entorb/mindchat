c99fd975a1202d56fda43f9fe7ef2ea273cb6857
Torben <59419684+entorb@users.noreply.github.com>
2026-01-07 09:21:49 +0100

    User can choose LLM and model

--- src/main.py
+++ src/main.py
@@ -4,12 +4,13 @@ import logging
 
 import streamlit as st
 
-from config import ENV, LLM_MODEL, LLM_PROVIDER
+from config import ENV, LLM_PROVIDERS
 from helper import (
     create_navigation,
     init_logging,
     show_login_page,
 )
+from llm import get_llm_provider
 
 # must be first Streamlit command
 st.set_page_config(
@@ -29,7 +30,35 @@ def main() -> None:  # noqa: D103
         return
 
     _page = create_navigation()
-    st.sidebar.markdown(f"*KI: {LLM_PROVIDER}, {LLM_MODEL}*")
+
+    # LLM provider select
+    if "LLM_PROVIDER" not in st.session_state:
+        st.session_state["LLM_PROVIDER"] = LLM_PROVIDERS[0]
+
+    default_index = LLM_PROVIDERS.index(st.session_state["LLM_PROVIDER"])
+    sel_provider = st.sidebar.selectbox(
+        "LLM",
+        LLM_PROVIDERS,
+        index=default_index,
+    )
+    # Handle provider change
+    if sel_provider != st.session_state["LLM_PROVIDER"]:
+        st.session_state["LLM_PROVIDER"] = sel_provider
+        del st.session_state["LLM_MODEL"]
+        st.rerun()
+
+    llm_provider = get_llm_provider(provider=st.session_state["LLM_PROVIDER"])
+    models = llm_provider.models
+
+    # Model select
+    if "LLM_MODEL" not in st.session_state:
+        st.session_state["LLM_MODEL"] = models[0]
+
+    default_index = models.index(st.session_state["LLM_MODEL"])
+    sel_model = st.sidebar.selectbox("Modell", models, index=default_index)
+    if sel_model != st.session_state["LLM_MODEL"]:
+        st.session_state["LLM_MODEL"] = sel_model
+    del sel_provider, sel_model, default_index, models
 
 
 if __name__ == "__main__":
