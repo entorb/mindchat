4b62a6189c462f98c6f904f6fb2be198b0704d64
Torben <59419684+entorb@users.noreply.github.com>
2026-04-12 16:34:17 +0200

    Tools

---
+++ scripts/update.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# ensure we are in the root dir
+cd $(dirname $0)/..
+
+# exit upon error
+set -e
+
+uv remove numpy pandas pyarrow streamlit google-genai mistralai openai pydantic
+uv remove --dev ruff pre-commit pytest pytest-cov tomli-w watchdog ollama
+
+uv lock --upgrade
+uv sync --upgrade
+
+# pin to old versions due to Uberspace restrictions
+uv add numpy==2.2.3 pandas==2.2.3 pyarrow==20.0.0 streamlit google-genai mistralai openai pydantic
+uv add --dev ruff pre-commit pytest pytest-cov tomli-w watchdog ollama
+
+uv lock --upgrade
+uv sync --upgrade
+
+python scripts/gen_requirements.py
+
+# ruff
+uv run ruff format
+uv run ruff check --fix
+
+# pre-commit
+uv run pre-commit autoupdate
+uv run pre-commit run --all-files
+
+echo DONE
